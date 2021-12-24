099f9cc603b0fb42040478585725b97de34f1122
headinghome330 <ccddcho@gmail.com>
2021-12-24 13:15:27 +0800

    更新issue_1[F

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_1.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_1.java
@@ -2,68 +2,42 @@ package us.dontcareabout.ccddcho.basic;
 
 public class Issue_1 {
 	public static void main(String[] args) {
-		forLoop forLoop = new forLoop();
-		System.out.println();
-		doWhileLoop doWhile = new doWhileLoop(5, 0, 0);
-		System.out.println();
-		whileLoop whileLoop = new whileLoop(5, 0, 0);
-	}
-}
+		int n = 5;
+		int i = 0, j = 0;
 
-class whileLoop {
-	int n = 5;
-	int i = 0;
-	int j = 0;
-	
-	public whileLoop(int n, int i, int j) {
-		this.n = n;
-		this.i = i;
-		this.j = j;
+		System.out.println("whileLoop 金字塔");
 		
 		while (i < n) {
 			while (j <= i) {
 				System.out.print("*");
 				j++;
 			}
-			j = this.j;
-//			j = 0;
+			j = 0;
 			i++;
 			System.out.println();
 		}
-	}
-}
 
-class doWhileLoop {
-	private int n;
-	private int i;
-	private int j;
-
-	public doWhileLoop(int n, int i, int j) {
-		this.n = n;
-		this.i = i;
-		this.j = j;
+		System.out.println("doWhileLoop 金字塔");
+		
+		i = 0;
 		do {
 			do {
 				j++;
 				System.out.print("*");
-
 			} while (j <= i);
-			j = this.j; //要放在 Constructor 是因為這邊的原因嗎 (不確定 xdd) 
-//			j = 0;
+			j = 0;
 			i++;
 			System.out.println();
 		} while (i < n);
-	}
-}
 
-class forLoop {
-	int n = 5;
-	{
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j <= i; j++) {
+		System.out.println("forLoop 金字塔");
+		
+		for (int a = 0; a < n; a++) {
+			for (int b = 0; b <= a; b++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+		
 	}
 }
