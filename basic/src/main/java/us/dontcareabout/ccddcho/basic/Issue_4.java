dc7502e4a55879f697a75676054fc7e2bff11eb2
headinghome330 <ccddcho@gmail.com>
2021-12-26 23:21:43 +0800

    新增：Issue_4

---
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_4.java
@@ -0,0 +1,14 @@
+package us.dontcareabout.ccddcho.basic;
+
+public class Issue_4 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		int i = 0;
+		int j = 0;
+
+		System.out.println("垂直金字塔");
+
+	}
+
+}
