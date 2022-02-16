8878edf243145ac9f64733e12f22c7663f8f6d7e
headinghome330 <ccddcho@gmail.com>
2022-02-16 15:03:03 +0800

    修改：Fib1 為遞迴版本

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_15/Fib1.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_15/Fib1.java
@@ -1,7 +1,7 @@
 package us.dontcareabout.ccddcho.basic.Issue_15;
 
 /**
- * 1.不應該使用 field 在迴圈內運算，使用 local vaiable 就可以避免多餘程式碼
+ * 1.不應該使用 field 在迴圈內運算，使用 local vaiable 就可以避免多餘程式碼 
  * 2.在迴圈內做 if else 條件判斷，n >= 2 才需要用到迴圈條件
  */
 public class Fib1 {
@@ -15,28 +15,14 @@ public class Fib1 {
 	}
 
 	public int fibonacci(int n) {
-
-		if (n == 0) {
-			return a0;
-		}
-
-		if (n == 1) {
-			return a1;
-		}
-		
 		return calFibonacci(a0, a1, n);
 	}
 
 	private static int calFibonacci(int v1, int v2, int n) {
-		int result = 0;
-
-		for (int i = 2; i <= n; i++) {
-			result = v1 + v2;
-			v1 = v2;
-			v2 = result;
+		if (n == 0) {
+			return v1;
 		}
-
-		return result;
+		return calFibonacci(v2, v1 + v2, --n);
 	}
-
+	
 }
