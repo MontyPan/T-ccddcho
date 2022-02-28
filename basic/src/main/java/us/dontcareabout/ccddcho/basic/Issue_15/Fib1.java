385bbb8c85ed77413b90f99dec3d314b03c73b1d
headinghome330 <ccddcho@gmail.com>
2022-02-28 20:14:38 +0800

    修改：Issue_15

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_15/Fib1.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_15/Fib1.java
@@ -1,42 +1,39 @@
 package us.dontcareabout.ccddcho.basic.Issue_15;
 
-/**
- * 1.不應該使用 field 在迴圈內運算，使用 local vaiable 就可以避免多餘程式碼
- * 2.在迴圈內做 if else 條件判斷，n >= 2 才需要用到迴圈條件
- */
 public class Fib1 {
 
-	private final int a0;
-	private final int a1;
+	private final int s;
+	private final int f;
+	private int[] a = new int[2];
 
 	public Fib1(int a0, int a1) {
-		this.a0 = a0;
-		this.a1 = a1;
+		this.s = a0;
+		this.f = a1;
 	}
 
 	public int fibonacci(int n) {
-
-		if (n == 0) {
-			return a0;
-		}
-
-		if (n == 1) {
-			return a1;
-		}
+		a[0] = s;
+		a[1] = f;
 		
-		return calFibonacci(a0, a1, n);
-	}
+		if (n >= a.length) {
+			int[] newA = new int[n + 1];
 
-	private static int calFibonacci(int v1, int v2, int n) {
-		int result = 0;
+			for (int i = 0; i < a.length; i++) {
+				newA[i] = a[i];
+			}
 
-		for (int i = 2; i <= n; i++) {
-			result = v1 + v2;
-			v1 = v2;
-			v2 = result;
+			for (int i1 = a.length; i1 <= n; i1++) {
+				newA[i1] = newA[i1 - 2] + newA[i1 - 1];
+				if (i1 == n) {
+					return newA[i1];
+				}
+			}
+
+			a = newA;
+		} else {
+			return a[n];
 		}
 
-		return result;
+		return -1;
 	}
-
 }
