58b915eb0e0138d35634f8f03e2a9403116ba754
headinghome330 <ccddcho@gmail.com>
2022-03-07 11:59:52 +0800

    修改 Fib1 為 arraylist 版本

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_15/Fib1.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_15/Fib1.java
@@ -1,39 +1,32 @@
 package us.dontcareabout.ccddcho.basic.Issue_15;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Fib1 {
 
-	private final int s;
-	private final int f;
-	private int[] a = new int[2];
+	ArrayList<Integer> list = new ArrayList<>();
 
 	public Fib1(int a0, int a1) {
-		this.s = a0;
-		this.f = a1;
+		list = new ArrayList<>(Arrays.asList(a0, a1));
 	}
 
 	public int fibonacci(int n) {
-		a[0] = s;
-		a[1] = f;
+
+		if (n == 0) {
+			return list.get(n);
+		}
 		
-		if (n >= a.length) {
-			int[] newA = new int[n + 1];
+		if (n >= list.size()) {
 
-			for (int i = 0; i < a.length; i++) {
-				newA[i] = a[i];
+			for (int i = list.size(); i <= n; i++) {
+				Integer result ;
+				result = list.get(i - 2) + list.get(i - 1);
+				list.add(i, result);
 			}
 
-			for (int i1 = a.length; i1 <= n; i1++) {
-				newA[i1] = newA[i1 - 2] + newA[i1 - 1];
-				if (i1 == n) {
-					return newA[i1];
-				}
-			}
-
-			a = newA;
-		} else {
-			return a[n];
 		}
 
-		return -1;
+		return list.get(list.size() - 1);
 	}
 }
