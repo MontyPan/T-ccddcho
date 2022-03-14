aa1ed149362986698456b01ed7707eb93f316a1b
headinghome330 <ccddcho@gmail.com>
2022-03-14 16:17:58 +0800

    修改 dailyFeeCtor()

--- basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_44/dailyFeeCtor.java
+++ basic/src/main/java/us/dontcareabout/ccddcho/basic/Issue_44/dailyFeeCtor.java
@@ -2,11 +2,14 @@ package us.dontcareabout.ccddcho.basic.Issue_44;
 
 public class dailyFeeCtor {
 	final int money;
-	Breakfast breakfastFee = new Breakfast();
-	Lunch lunchFee = new Lunch();
-	Dinner dinnerFee = new Dinner();
+	Breakfast breakfastFee;
+	Lunch lunchFee;
+	Dinner dinnerFee;
 	
 	public dailyFeeCtor() {
+		breakfastFee = new Breakfast();
+		lunchFee = new Lunch();
+		dinnerFee = new Dinner(); 
 		money = breakfastFee.Fee() + lunchFee.Fee() + dinnerFee.Fee();
 	}
 
